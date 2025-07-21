8adb4984e40cc712b9cbf288734fa9f9d65e99f5
Pranav-un <pranavunnikrishnan109@gmail.com>
2025-07-21 17:43:21 +0530

    Removed and added new files

---
+++ backend/src/main/java/com/noteguard/NoteGuardApplication.java
@@ -0,0 +1,13 @@
+package com.noteguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteGuardApplication.class, args);
+	}
+
+}
